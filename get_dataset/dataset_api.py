2fbd3f6b42f2a94ec296923fa80dad0223e14748
cosimochetta <minochetta@gmail.com>
2020-11-07 16:44:00 +0100

    feat: updated ERA5_land parameters

--- get_dataset/dataset_api.py
+++ get_dataset/dataset_api.py
@@ -46,35 +46,34 @@ def get_era5_land():
     c = cdsapi.Client()
 
     c.retrieve(
-        'reanalysis-era5-land-monthly-means',
-        {
-            'product_type': 'monthly_averaged_reanalysis',
-            'variable': [
-                '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature',
-                '2m_temperature', 'forecast_albedo', 'leaf_area_index_high_vegetation',
-                'leaf_area_index_low_vegetation', 'skin_reservoir_content', 'skin_temperature',
-                'soil_temperature_level_1', 'soil_temperature_level_2', 'soil_temperature_level_3',
-                'soil_temperature_level_4', 'surface_latent_heat_flux', 'surface_net_solar_radiation',
-                'surface_net_thermal_radiation', 'surface_pressure', 'surface_sensible_heat_flux',
-                'surface_solar_radiation_downwards', 'surface_thermal_radiation_downwards', 'total_precipitation',
-                'volumetric_soil_water_layer_1', 'volumetric_soil_water_layer_2', 'volumetric_soil_water_layer_3',
-                'volumetric_soil_water_layer_4',
-            ],
-            'year': '2019',
-            'month': [
-                '01', '02', '03',
-                '04', '05', '06',
-                '07', '08', '09',
-                '10', '11', '12',
-            ],
-            'time': '00:00',
-            'area': [
-                70, 10, 35,
-                30,
-            ],
-            'format': 'netcdf',
-        },
-        'ERA5_land.nc')
+    'reanalysis-era5-land-monthly-means',
+    {
+        'product_type': 'monthly_averaged_reanalysis',
+        'variable': [
+            '2m_dewpoint_temperature', '2m_temperature', 'forecast_albedo',
+            'leaf_area_index_high_vegetation', 'leaf_area_index_low_vegetation', 'skin_reservoir_content',
+            'skin_temperature', 'soil_temperature_level_1', 'soil_temperature_level_2',
+            'soil_temperature_level_3', 'soil_temperature_level_4', 'surface_latent_heat_flux',
+            'surface_net_solar_radiation', 'surface_net_thermal_radiation', 'surface_pressure',
+            'surface_solar_radiation_downwards', 'surface_thermal_radiation_downwards', 'total_precipitation',
+            'volumetric_soil_water_layer_1', 'volumetric_soil_water_layer_2', 'volumetric_soil_water_layer_3',
+            'volumetric_soil_water_layer_4',
+        ],
+        'year': '2019',
+        'month': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+        ],
+        'time': '00:00',
+        'area': [
+            70, 10, 35,
+            30,
+        ],
+        'format': 'netcdf',
+    },
+    'ERA5_land.nc')
 
 def get_era5_data():
     
